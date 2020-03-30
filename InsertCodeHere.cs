e3c4a17b4ca24ae26ab373859c9535be6587f16a
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:34:51 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,17 @@
+﻿namespace DiceGame
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            DiceCup mydicecup = new DiceCup();
+            System.Console.WriteLine($"The 2 dices rolled {mydicecup.TotalValue}");
+
+            mydicecup.Shake();
+            System.Console.WriteLine($"The 2 dices rolled {mydicecup.TotalValue}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
